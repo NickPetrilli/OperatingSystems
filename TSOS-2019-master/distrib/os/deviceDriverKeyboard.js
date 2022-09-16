d274339cdd6fb223040be556cec1135697108933
NickPetrilli <Nicholas.Petrilli1@marist.edu>
2022-09-15 23:22:41 -0400

    Attempting to allow special characters

--- TSOS-2019-master/distrib/os/deviceDriverKeyboard.js
+++ TSOS-2019-master/distrib/os/deviceDriverKeyboard.js
@@ -39,12 +39,69 @@ var TSOS;
                 // TODO: Check for caps-lock and handle as shifted if so.
                 _KernelInputQueue.enqueue(chr);
             }
-            else if (((keyCode >= 48) && (keyCode <= 57)) || // digits
-                (keyCode == 32) || // space
-                (keyCode == 13)) { // enter
+            else if ((keyCode >= 48) && (keyCode <= 57)) {
+                if (isShifted == true) {
+                    switch (keyCode) {
+                        case 48:
+                            chr = ")";
+                            break;
+                        case 49:
+                            chr = "!";
+                            break;
+                        case 50:
+                            chr = "@";
+                            break;
+                        case 51:
+                            chr = "#";
+                            break;
+                        case 52:
+                            chr = "$";
+                            break;
+                        case 53:
+                            chr = "%";
+                            break;
+                        case 54:
+                            chr = "^";
+                            break;
+                        case 55:
+                            chr = "&";
+                            break;
+                        case 56:
+                            chr = "*";
+                            break;
+                        case 57:
+                            chr = "(";
+                            break;
+                    }
+                }
+                else {
+                    chr = String.fromCharCode(keyCode);
+                }
+                _KernelInputQueue.enqueue(chr);
+            }
+            else if ((keyCode == 32) || (keyCode == 13)) {
                 chr = String.fromCharCode(keyCode);
                 _KernelInputQueue.enqueue(chr);
             }
+            /*
+            ||   // digits
+                        (keyCode == 32)                     ||   // space
+                        (keyCode == 13)                     ||   // enter
+                        (keyCode == 8)                      ||   // backspace
+                        (keyCode == 188)                    ||   // ,
+                        (keyCode == 190)                    ||   // .
+                        (keyCode == 191)                    ||   // /
+                        (keyCode == 186)                    ||   // ;
+                        (keyCode == 222)                    ||   // '
+                        (keyCode == 219)                    ||   // [
+                        (keyCode == 221)                    ||   // ]
+                        (keyCode == 189)                    ||   // -
+                        (keyCode == 187)) {                      // =
+                            
+                chr = String.fromCharCode(keyCode);
+                _KernelInputQueue.enqueue(chr);
+            }
+            */
         }
     }
     TSOS.DeviceDriverKeyboard = DeviceDriverKeyboard;
